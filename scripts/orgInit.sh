f776825474e9b58332542834b6075fafa1d018ba
muenzpraeger <rwinkelmeyer@salesforce.com>
2018-05-09 07:48:52 +0200

    Minor updates

--- scripts/orgInit.sh
+++ scripts/orgInit.sh
@@ -6,7 +6,7 @@ if [ "$#" -eq 1 ]; then
   DURATION=$1
 fi
 
-sfdx force:org:create -a <appalias> -s -f config/project-scratch-def.json -f DURATION
+sfdx force:org:create -a nto -s -f config/project-scratch-def.json -d $DURATION
 sfdx force:source:push
 sfdx force:user:permset:assign -n nto
 sfdx force:data:tree:import --plan ./data/Merchandise__c-plan.json
