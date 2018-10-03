a2e4d139d21752bc5944c2863d03008df18e661f
Christophe Coenraets <ccoenraets@gmail.com>
2018-10-03 07:07:52 -0400

    replacing custom cometd with empAPI in EinsteinTwitterFeed

--- force-app/main/default/aura/EinsteinTwitterFeed/EinsteinTwitterFeedController.js
+++ force-app/main/default/aura/EinsteinTwitterFeed/EinsteinTwitterFeedController.js
@@ -1,16 +1,21 @@
 ({
-    messageHandler: function (component, event) {
-        var message = event.getParam("message");
-        try {
-            var tweets = component.get("v.tweets");
-            var tweet = message.data.payload;
-            tweet.Sentiment_Predictions__c = JSON.parse(tweet.Sentiment_Predictions__c);
-            tweet.Intent_Predictions__c = JSON.parse(tweet.Intent_Predictions__c);
-            tweet.Vision_Predictions__c = JSON.parse(tweet.Vision_Predictions__c);
-            tweets.push(message.data.payload);
-            component.set("v.tweets", tweets);
-        } catch (e) {
-            console.log(e);
-        }
+    subscribe: function (component, event) {
+        var empApi = component.find("empApi");
+        var channel = component.get("v.channel");
+        var replayId = -2;
+        empApi.subscribe(channel, replayId, $A.getCallback(function (message) {
+            try {
+                var tweets = component.get("v.tweets");
+                var tweet = message.data.payload;
+                tweet.Sentiment_Predictions__c = JSON.parse(tweet.Sentiment_Predictions__c);
+                tweet.Intent_Predictions__c = JSON.parse(tweet.Intent_Predictions__c);
+                tweet.Vision_Predictions__c = JSON.parse(tweet.Vision_Predictions__c);
+                tweets.push(message.data.payload);
+                component.set("v.tweets", tweets);
+            } catch (e) {
+                console.log(e);
+            }
+        }));
     }
+
 })
